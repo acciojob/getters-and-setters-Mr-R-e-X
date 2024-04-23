13909d55096c79a1eb845ff0fbcc5d26a4c61d0f
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-23 12:33:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 class Person {
 	constructor(name, age){
-		this.name = name;
-		this.age = age
+		this._name = name;
+		this._age = age
 	}
     get name(){
-        return this.name;
+        return this._name;
     }
-    set age(newAge){
-        this.age = newAge;
+    set age(age){
+        this._age = age;
         // return this.age;
     }
 }
